95d3c611665b8617654266f55d0d73c2389286d9
alstjd0930 <alstjd0930@naver.com>
2023-08-03 11:31:23 +0900

    .

--- semi/src/main/java/semi/project/controller/MemberListServlet.java
+++ semi/src/main/java/semi/project/controller/MemberListServlet.java
@@ -1,6 +1,8 @@
 package semi.project.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,22 +10,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/user/index")
-public class UserController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-    public UserController() {
+/**
+ * Servlet implementation class ResutlController
+ */
+@WebServlet("/user/result")
+public class MemberListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MemberListServlet() {
         super();
     }
 
-	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/view/user/index.jsp").forward(request, response);
+		
+		request.getRequestDispatcher("/WEB-INF/view/user/result.jsp").forward(request, response);
+	
 	}
 
 
 //	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//	
+//		doGet(request, response);
 //	}
 
 }
